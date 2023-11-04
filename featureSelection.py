044a742eb1dc44964b66e15f75956c0a08fe6a78
felixzhao <felix628ft@126.com>
2023-11-04 18:27:50 +0800

    CITY ENCODER + MAGIC FEATURE

--- featureSelection.py
+++ featureSelection.py
@@ -1,19 +1,18 @@
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import itertools
-import numpy as np
 
-def create_feature_crosses(df, target_feature, num_new_features):
+def determine_feature_crosses(df, target_feature, num_new_features):
     """
-    Create new features by crossing the most important features.
+    Determine which feature crosses to create based on feature importances.
 
     Parameters:
     - df: pandas DataFrame containing the features and target.
-    - target_feature: string, the name of the target feature column.
-    - num_new_features: int, the number of new features to create.
+    - target_feature: the name of the target feature column.
+    - num_new_features: the number of new feature crosses to create.
 
     Returns:
-    - df: pandas DataFrame with the new features added.
+    - feature_pairs: a list of tuples, where each tuple contains a pair of feature names.
     """
     # Ensure the target feature is a column in the DataFrame
     if target_feature not in df.columns:
@@ -45,15 +44,25 @@ def create_feature_crosses(df, target_feature, num_new_features):
     # Get the top important features
     top_features = feature_importance_df['Feature'].iloc[:n].values
 
-    # Create new features by crossing the top important features
-    # Use itertools.combinations to create unique pairs
-    new_features_combinations = list(itertools.combinations(top_features, 2))
+    # Create a list of feature pairs to cross
+    feature_pairs = list(itertools.combinations(top_features, 2))[:num_new_features]
 
+    return feature_pairs
+
+def create_feature_crosses(df, feature_pairs):
+    """
+    Create new features by crossing the given pairs of features.
+
+    Parameters:
+    - df: pandas DataFrame containing the features.
+    - feature_pairs: list of tuples, each tuple contains a pair of features to cross.
+
+    Returns:
+    - df: pandas DataFrame with the new features added.
+    """
     # Now create the new features
-    for i, (feature_a, feature_b) in enumerate(new_features_combinations, 1):
-        new_feature_name = f'magic_{i}'
+    for i, (feature_a, feature_b) in enumerate(feature_pairs, 1):
+        new_feature_name = f'cross_{feature_a}_{feature_b}'
         df[new_feature_name] = df[feature_a] * df[feature_b]
-        if i == num_new_features:  # Stop after creating the desired number of new features
-            break
 
     return df
